2ff518f5a87b373fd59179f8f2364aed069fe522
swpv <80281721+FilipDlouhy@users.noreply.github.com>
2024-03-22 19:33:50 +0100

    Added initial calendar setup

--- src/components/main-components/calendar-components/CalendarContainer.tsx
+++ src/components/main-components/calendar-components/CalendarContainer.tsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+import CalendarToday from "./CalendarToday";
+import CalendarWeek from "./CalendarWeek";
+import { CalendarViewEnum } from "./helpers/CalendarViewEnum";
+import CalendarMoth from "./CalendarMoth";
+
 function CalendarContainer() {
-  return <div>CalendarContainer</div>;
+  const [calendarView, setCalendarView] = useState<CalendarViewEnum>(
+    CalendarViewEnum.DAY
+  );
+
+  const renderCalendarView = () => {
+    switch (calendarView) {
+      case CalendarViewEnum.DAY:
+        return <CalendarToday setCalendarView={setCalendarView} />;
+      case CalendarViewEnum.WEEK:
+        return <CalendarWeek setCalendarView={setCalendarView} />;
+      case CalendarViewEnum.MONTH:
+        return <CalendarMoth setCalendarView={setCalendarView} />;
+    }
+  };
+
+  return <div>{renderCalendarView()}</div>;
 }
 
 export default CalendarContainer;
